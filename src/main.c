77d491e23af7e229003f301f392014b290956fbf
Bamontan <baptiste.demontangon@gmail.com>
2021-02-14 16:33:34 +0100

    tested graph creation from adjency matrix

--- src/main.c
+++ src/main.c
@@ -1,6 +1,54 @@
 #include "../headers/all.h"
 
+#define SIZE 5
+#define cdouble(a) misc_createdouble(a, misc_printErr)
+#define rien cdouble(0)
+
+void* print_uint(void* num, void* args) {
+    printf("%u\n",*(uint*)num);
+    return NULL;
+}
+
+
 int main(int argc, char const *argv[])
 {
+    double* mat[SIZE][SIZE] = {
+        {NULL, rien, NULL, NULL, NULL},
+        {rien, NULL, rien, NULL, NULL},
+        {rien, NULL, NULL, NULL, rien},
+        {rien, NULL, NULL, NULL, rien},
+        {NULL, NULL, rien, rien, NULL}
+    };
+
+    double*** mat2 = malloc(sizeof(double**) * SIZE);
+    for(uint i = 0; i < SIZE; i++) {
+        mat2[i] = malloc(sizeof(double*) * SIZE);
+        for(uint j = 0; j < SIZE; j++) {
+            mat2[i][j] = mat[i][j];
+        }
+    }
+
+    void* values[SIZE];
+    for(uint i = 0; i < SIZE; i++) {
+        values[i] = misc_createuint(i + 1, misc_printErr);
+    }
+
+    graph_node** graph = graph_constructAdjency(SIZE, values, mat2);
+    graph_node* node = graph[2];
+    graph_DFS(node, print_uint, NULL);
+    printf("\n");
+    graph_BFS(node, print_uint, NULL);
+
+
+
+    graph_freeGraph(node, free);
+    free(graph);
+    for(uint i = 0; i < SIZE; i++) {
+        free(mat2[i]);
+        for(uint j = 0; j < SIZE; j++) {
+            free(mat[i][j]);
+        }
+    }
+    free(mat2);
     return 0;
 }
