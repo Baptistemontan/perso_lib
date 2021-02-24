539b445f3689c558dba03aa218f229ade6b8ad5e
Bamontan <baptiste.demontangon@gmail.com>
2021-02-24 23:48:06 +0100

    changed debug check to assert, can be disabled

--- src/dynarr.c
+++ src/dynarr.c
@@ -1,4 +1,5 @@
 #include "../headers/dynarr.h"
+#include <assert.h>
 
 #define SHIFT(n) (1 << n) // fast 2^n
 #define SORT() qsort(darray_info->arr, darray_info->size, sizeof(dynarr_arr*), dynarr_private_comp_sort)
@@ -36,13 +37,13 @@ static int dynarr_private_comp_sort(const void* a, const void* b) {
     return (da->baseArr > db->baseArr) - (da->baseArr < db->baseArr);
 }
 
-static size_t dynarr_private_findArr(void* arr) {
-    if(arr == NULL) return 0;
+static size_t dynarr_private_findArr(void* arr_fa) {
+    assert(arr_fa == NULL);
     size_t a = 0, b = darray_info->size - 1, mid;
     while(a <= b && b < darray_info->size) {
         mid = (a + b) >> 1;
-        if(darray[mid]->arr == arr) return mid + 1;
-        if(darray[mid]->arr > arr) {
+        if(darray[mid]->arr == arr_fa) return mid + 1;
+        if(darray[mid]->arr > arr_fa) {
             b = mid - 1;
         } else {
             a = mid + 1;
@@ -169,15 +170,14 @@ void dynarr_free(void* arr) {
         free(darray_info);
         darray_info = NULL;
         darray = NULL;
-        // printf("no more bitch\n");
     }
 }
 
-static void dynarr_private_popBack(dynarr_arr* arr) {
-    if(arr->size == 0) return;
-    SAVEBUFF(arr->arr + (arr->size - 1) * arr->byteSize, arr->byteSize);
-    arr->size--;
-    dynarr_private_reduce(arr);
+static void dynarr_private_popBack(dynarr_arr* arr_pb) {
+    assert(arr_pb->size == 0);
+    SAVEBUFF(arr_pb->arr + (arr_pb->size - 1) * arr_pb->byteSize, arr_pb->byteSize);
+    arr_pb->size--;
+    dynarr_private_reduce(arr_pb);
 }
 
 void* dynarr_popBack(void* arrAdd) {
@@ -191,13 +191,13 @@ void* dynarr_popBack(void* arrAdd) {
     return tmpBuff;
 }
 
-static void dynarr_private_popFront(dynarr_arr* arr) {
-    if(arr->size == 0) return;
-    SAVEBUFF(arr->arr, arr->byteSize);
-    arr->size--;
-    arr->offset++;
-    arr->arr = arr->baseArr + (arr->offset * arr->byteSize);
-    dynarr_private_reduce(arr);
+static void dynarr_private_popFront(dynarr_arr* arr_pf) {
+    assert(arr_pf->size == 0);
+    SAVEBUFF(arr_pf->arr, arr_pf->byteSize);
+    arr_pf->size--;
+    arr_pf->offset++;
+    arr_pf->arr = arr_pf->baseArr + (arr_pf->offset * arr_pf->byteSize);
+    dynarr_private_reduce(arr_pf);
 }
 
 void* dynarr_popFront(void* arrAdd) {
